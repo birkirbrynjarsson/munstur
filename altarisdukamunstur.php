e66a5d95dee05d40f1f0335f221122de7ce913c4
Birkir Brynjarsson <birkir@birkir.is>
2017-05-28 11:10:55 +0000

    Added new page, altarisdukamunstur

---
+++ altarisdukamunstur.php
@@ -0,0 +1,52 @@
+<?php include_once('header.html') ?>
+
+<body id="subpage">
+<div class="wrap_all">
+    <div id="top">
+	  <div id="head">
+
+            <?php include_once('navigation.html') ?>
+
+			 <p class="breadcrumb">
+          	<span class="breadcrumb_info">Þú ert hér:</span>
+            <a href="http://munstur.is/index.php">Munstur.is</a><span> » </span><span class="current_crumb">Altarisdúkar </span></p>
+    	</div><!-- end head-->
+
+        <div id="main">
+
+          <div id="content" class="bg_sidebar">
+           <div id="inner_content">
+
+           <div class="entry">
+
+           <!--<span class="meta">Um Verkefnið</span>-->
+           <h2><a href="#" id="altarisdukar">Altarisdúkamunstur</a></h2>
+
+           <div class="entry-head">
+               <!--<span class="categories"><a href="#">twicet</a>,<a href="#">Startup</a></span>-->
+               <!--<span class="date">17. júlí 2010</span>-->
+               <!--<span class="comments"><a href="#">3 Comments</a></span>-->
+               <!--<h3>Verkefnið Altarisdúkar</h3>-->
+           </div>
+           <div class="entry-content">
+
+           	   <p><a href="/images/altarisdukar/seria/1.jpg" rel="prettyPhoto[altarisdukar]"><img class="aligncenter" src="/images/altarisdukar/seria/1-tn.jpg" alt="" /></a></p>
+
+			</div><!--end entry-content-->
+			</div><!--end entry-->
+
+           </div><!-- end inner_content-->
+
+           <?php include_once('sidebar.html') ?>
+
+          </div><!-- end content-->
+
+      </div><!--end main-->
+
+        <?php include_once('footer.html') ?>
+
+    </div><!--end top-->
+</div><!--end wrapp_all-->
+</body>
+
+</html>
