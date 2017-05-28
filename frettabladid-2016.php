65e04d8ad220267b4965595d5cbe51cc24c40009
Birkir Brynjarsson <birkir@birkir.is>
2017-05-28 11:45:55 +0000

    Article in frettabladid added

--- frettabladid-2016.php
+++ frettabladid-2016.php
@@ -22,6 +22,28 @@
            <span class="meta">Grein sem birtist í Fréttablaðinu 3. desember 2016.</span>
            <h2>Kærleikur og virðing tengjast oft gerð altarisdúka</h2>
 
+           <div class="entry-head">
+               <p>Upphaflega birt í Fréttablaðinu og á <a href="http://www.visir.is/g/2016161209649/kaerleikur-og-virding-tengjast-oft-gerd-altarisduka">Vísi</a>.</p>
+               <span class="date">3. desember 2016</span><br/>
+               <span class="date">Gunnþóra Gunnarsdóttir skrifar</span>
+           </div>
+
+            <div class="entry-content">
+
+                <p>Mín áhugamál tengjast handverki að mestu, enda er ég alin upp við handverk,“ segir Jenný Karlsdóttir, fyrrverandi kennari sem vinnur að því að mynda og skrá altarisdúka í öllum kirkjum landsins.  „Pabbi var bólstrari og mamma eina íslenska konan sem lauk námi í körfu­mublusmíði. Einn vetur var ég í Noregi í skóla og stúderaði myndvefnað, fór mikið á sýningar og í kirkjur og einhverra hluta vegna fékk ég áhuga á altarisdúkum enda hafði ég lengi safnað textíl og sýslað með munstur.</p>
+                <p>Ég hafði samband við vinkonu mína, Oddnýju E. Magnúsdóttur á Húsavík, sem er þjóðfræðingur og spurði hvort hún væri ekki til í að koma með mér í íslenskar kirkjur að skoða altarisdúka og hún var það. Þannig fór snjóbolti af stað.</p>
+                <p>Við Oddný erum búnar að kemba Norðurlandið, Borgarfjörð, Árnessýslu og Fljótsdalshérað og fara þar í hátt í 200 kirkjur. Ég gæti trúað að dúkarnir væru orðnir um 300 því sumar kirkjur eiga gamla dúka, aflagða og okkur finnst mikill fengur í að fá að skoða þá og mynda. Sumir eru líka komnir á söfn í sínum heimahéruðum og hefur jafnvel verið bjargað úr ruslapokum,“ segir Jenný og tekur fram að þær Oddný séu fyrst og fremst að rannsaka munstrin, verkefnið komi trúmálum ekkert við.</p>
+                <p>Jenný segir aðallega þrjár hannyrðaaðferðir notaðar við gerð altarisdúka, hekl, harðangurssaum og feneyjasaum. Einnig séu dúkar handofnir og gerðir með fleiri aðferðum. „Dúkar með feneyjasaumi eru yfirleitt gamlir. Þeir elstu sem við höfum rekist á eru um það bil aldargamlir og nokkrir enn í notkun. Það er erfitt að nálgast munstur á slíkum dúkum nú orðið.“</p>
+                <p>Þær Jenný og Oddný sækjast eftir að skrá aldur og vinnslusögu dúkanna og af hvaða tilefni þeir hafa komist í eigu viðkomandi kirkna. Þar styðjast þær einkum við upplýsingar heimafólks sem þær hitta á ferðum sínum en ýmislegt er líka skráð í kirkjubækur.</p>
+                <p>„Kærleikur og virðing tengjast oft gerð altarisdúka, í mörgum tilfellum eru þeir minningargjafir. Við vitum dæmi um að fleiri en einn ættliður hefur sameinast um að gera dúk, jafnvel heilt kvenfélag þannig að dúkur hafi gengið á milli kvenna þar til saumaskap var lokið. Til er líka að kona hafi séð fyrir sér rúmlegu eða rólegheit í einhvern tíma og þá notað tímann í að gera altarisdúk. Svo hafa nokkrar konur gert marga altarisdúka. Ein þeirra var Anna frá Moldnúpi undir Eyjafjöllum, við höfum víða rekist á dúka eftir hana.“</p>
+                <p>Altarisdúkarnir eru eftir konur í langflestum tilfellum, að sögn Jennýjar. „Á öllu Norðurlandi vitum við bara um einn dúk sem karlmaður tók þátt í að gera með því að teikna munstur fyrir konu sína. Á sumum öðrum svæðum koma karlmenn örlítið meira við sögu.“</p>
+                <p>Dúkarnir hafa varðveist misjafnlega. Jenný segir þá einkum eiga þrjá óvini. Þvottavélar og þurrkarar eru þar algerlega í fyrsta sæti. Sólin er óvinur númer tvö og svo eru það mýsnar. „Eina gamla konu hitti ég sem hafði jafnan tólgarmola eða kerti einhvers staðar undir kirkjubekk handa músunum svo þær gæddu sér síður á dúknum.“</p>
+                <p>Jenný segir alltaf gaman að sjá ný munstur og dúk sem fer vel á altari kirkjunnar. „Það þarf öðru vísi munstur í stórar kirkjur en litlar og verður að huga að því að hönnun dúks falli vel að hverri kirkju fyrir sig.</p>
+                <p>Spurð hvort hún telji um listaverk að ræða þar sem altarisdúkar séu segir Jenný að minnsta kosti listahandverk á sumum þeirra. „Svo hafa listakonur líka hannað og unnið dúka sem eru þá einstakir,“ segir hún og getur þess að áhugi virðist á margs konar handverki í dag og vonandi verði svo áfram.</p>
+                <p>En sitja íslenskar konur enn við að bródera eða hekla altarisdúka? „Já, já, það er víða verið að gera dúka. Til dæmis er kominn nýr og fallegur dúkur í Reykholtskirkju í Borgarfirði sem Helga Magnúsdóttir saumaði og líka kom sérhannaður dúkur eftir Bryndísi Símonardóttur í Saurbæjarkirkju í Eyjafirði á þessu ári. Mjög merkilegt framtak,“ segir Jenný.</p>
+                <p>Næst er hún spurð hvað þær stöllur ætli að gera við upplýsingarnar sem þær eru að safna um altarisdúka landsins. Er þetta doktorsverkefni?<br/>„Ja, ekki er það beint á dagskrá. Áður en farið er að skoða svoleiðis þarf rannsóknin að ná yfir allt landið. Við eigum Breiðafjörð, Snæfellsnes, Vestfirði, höfuðborgarsvæðið, Rangárvallasýslu, Skaftafellssýslur og Austfirði eftir.</p>
+                <p>Stundum finnst okkur við vera í kapphlaupi við tímann við að afla upplýsinga um gamla altarisdúka. Við settum okkur markmið í upphafi. Það fyrsta var að skoða munstur á altarisdúkum, annað var að hafa gaman af því og þriðja var að gera okkur þetta að kostnaðarlausu.</p>
+                <p>Í upphafi sóttum við um styrk til Kristnihátíðarsjóðs og kom hann okkur vel af stað. Ýmsir fleiri menningarsjóðir hafa styrkt verkefnið og erum við þakklátar fyrir. Við lifum mjög spart á ferðalögum okkar, en bensínkostnaður er óhjákvæmilegur. Auðvitað dreymir okkur um fjármagn til að halda áfram þessari vinnu.<p>
 
  			</div><!--end entry-content-->
 			</div><!--end entry-->
