8a8b1c762d6610d2cd733954deeed28f04f82e16
Birkir Brynjarsson <birkir@birkir.is>
2017-05-29 14:30:41 +0000

    wip portfolio sidebar15

--- altarisdukamunstur.php
+++ altarisdukamunstur.php
@@ -47,7 +47,7 @@
                     <div class="portfolio_item_noside">
                     <a href="images/altarisdukar/munstur/dukur1.jpg"><img src="images/altarisdukar/munstur/dukur1_tn.jpg" alt="" /></a>
                     </div>
-                    <p>Hægri-smelltu á myndina og veldu <i><b>Save link as...</b></i> til þess að vist munstrið á tölvuna þína.</p>
+                    <p>Hægri-smelltu á myndina og veldu <i><b>Save link as...</b></i> til þess að vista munstrið á tölvuna þína.</p>
                 </div><!--end small_box-->
 			</div>
 
@@ -64,7 +64,7 @@
                     <div class="portfolio_item_noside">
                     <a href="images/altarisdukar/munstur/dukur2.jpg"><img src="images/altarisdukar/munstur/dukur2_tn.jpg" alt="" /></a>
                     </div>
-                    <p>Hægri-smelltu á myndina og veldu <i><b>Save link as...</b></i> til þess að vist munstrið á tölvuna þína.</p>
+                    <p>Hægri-smelltu á myndina og veldu <i><b>Save link as...</b></i> til þess að vista munstrið á tölvuna þína.</p>
                 </div><!--end small_box-->
 			</div>
 
@@ -81,7 +81,7 @@
                     <div class="portfolio_item_noside">
                     <a href="images/altarisdukar/munstur/dukur3.jpg"><img src="images/altarisdukar/munstur/dukur3_tn.jpg" alt="" /></a>
                     </div>
-                    <p>Hægri-smelltu á myndina og veldu <i><b>Save link as...</b></i> til þess að vist munstrið á tölvuna þína.</p>
+                    <p>Hægri-smelltu á myndina og veldu <i><b>Save link as...</b></i> til þess að vista munstrið á tölvuna þína.</p>
                 </div><!--end small_box-->
 			</div>
 
@@ -98,7 +98,7 @@
                     <div class="portfolio_item_noside">
                     <a href="images/altarisdukar/munstur/dukur4.jpg"><img src="images/altarisdukar/munstur/dukur4_tn.jpg" alt="" /></a>
                     </div>
-                    <p>Hægri-smelltu á myndina og veldu <i><b>Save link as...</b></i> til þess að vist munstrið á tölvuna þína.</p>
+                    <p>Hægri-smelltu á myndina og veldu <i><b>Save link as...</b></i> til þess að vista munstrið á tölvuna þína.</p>
                 </div><!--end small_box-->
 			</div>
 
@@ -115,7 +115,7 @@
                     <div class="portfolio_item_noside">
                     <a href="images/altarisdukar/munstur/dukur5.jpg"><img src="images/altarisdukar/munstur/dukur5_tn.jpg" alt="" /></a>
                     </div>
-                    <p>Hægri-smelltu á myndina og veldu <i><b>Save link as...</b></i> til þess að vist munstrið á tölvuna þína.</p>
+                    <p>Hægri-smelltu á myndina og veldu <i><b>Save link as...</b></i> til þess að vista munstrið á tölvuna þína.</p>
                 </div><!--end small_box-->
 			</div>
 
