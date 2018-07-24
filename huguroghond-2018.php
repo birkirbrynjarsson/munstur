206812a852a898acc312c8fc1883d16c0cec6629
Birkir Brynjarsson <birkir@birkir.is>
2018-07-24 16:30:41 +0000

    Add new article sub page

---
+++ huguroghond-2018.php
@@ -0,0 +1,61 @@
+<?php include_once('header.html') ?>
+
+<body id="subpage">
+<div class="wrap_all">
+    <div id="top">
+	  <div id="head">
+
+            <?php include_once('navigation.html') ?>
+
+			 <p class="breadcrumb">
+          	<span class="breadcrumb_info">Þú ert hér:</span>
+            <a href="http://munstur.is/index.php">Munstur.is</a><span> » </span><span class="current_crumb">Altarisdúkar </span></p>
+    	</div><!-- end head-->
+
+        <div id="main">
+
+          <div id="content" class="bg_sidebar">
+           <div id="inner_content">
+
+            <div class="entry">
+
+           <span class="meta">Grein sem birtist í Hugur og Hönd 2018.</span>
+           <h2>Dagarnir eru aldrei nógu langir, viðtal við Jenný Karlsdóttur</h2>
+
+           <div class="entry-head">
+               <!--<span class="categories"><a href="#">twicet</a>,<a href="#">Startup</a></span>-->
+               <span class="date">24. júlí 2018</span><br />
+               <span class="date">Smelltu á myndina og svo á boxið efst í hægra horninu á myndinni til að sjá hana í fullri stærð.</span>
+               <!--<span class="comments"><a href="#">3 Comments</a></span>-->
+           </div>
+
+           <div class="entry-content"><!--<img class="aligncenter" src="files/contentpic1.jpg" alt="" />-->
+           <a href="images/altarisdukar/huguroghond-2018/huguroghond-2018-1.jpg" rel="prettyPhoto[altaris]"><img src="images/altarisdukar/huguroghond-2018/huguroghond-2018-1_tn.jpg" alt="Grein í Hugur og Hönd 2018, bls.9" width="570" /></a>
+           <a href="images/altarisdukar/huguroghond-2018/huguroghond-2018-2.jpg" rel="prettyPhoto[altaris]"><img src="images/altarisdukar/huguroghond-2018/huguroghond-2018-2_tn.jpg" alt="Grein í Hugur og Hönd 2018, bls.10" width="570" /></a>
+           <a href="images/altarisdukar/huguroghond-2018/huguroghond-2018-3.jpg" rel="prettyPhoto[altaris]"><img src="images/altarisdukar/huguroghond-2018/huguroghond-2018-3_tn.jpg" alt="Grein í Hugur og Hönd 2018, bls.11" width="570" /></a>
+           <a href="images/altarisdukar/huguroghond-2018/huguroghond-2018-4.jpg" rel="prettyPhoto[altaris]"><img src="images/altarisdukar/huguroghond-2018/huguroghond-2018-4_tn.jpg" alt="Grein í Hugur og Hönd 2018, bls.12" width="570" /></a>
+           <a href="images/altarisdukar/huguroghond-2018/huguroghond-2018-5.jpg" rel="prettyPhoto[altaris]"><img src="images/altarisdukar/huguroghond-2018/huguroghond-2018-5_tn.jpg" alt="Grein í Hugur og Hönd 2018, bls.13" width="570" /></a>
+           <a href="images/altarisdukar/huguroghond-2018/huguroghond-2018-6.jpg" rel="prettyPhoto[altaris]"><img src="images/altarisdukar/huguroghond-2018/huguroghond-2018-6_tn.jpg" alt="Grein í Hugur og Hönd 2018, bls.14" width="570" /></a>
+           <a href="images/altarisdukar/huguroghond-2018/huguroghond-2018-7.jpg" rel="prettyPhoto[altaris]"><img src="images/altarisdukar/huguroghond-2018/huguroghond-2018-7_tn.jpg" alt="Grein í Hugur og Hönd 2018, bls.15" width="570" /></a>
+
+
+ 			</div><!--end entry-content-->
+			</div><!--end entry-->
+
+
+
+           </div><!-- end inner_content-->
+
+           <?php include_once('sidebar.html') ?>
+
+          </div><!-- end content-->
+
+      </div><!--end main-->
+
+        <?php include_once('footer.html') ?>
+
+    </div><!--end top-->
+</div><!--end wrapp_all-->
+</body>
+
+</html>
