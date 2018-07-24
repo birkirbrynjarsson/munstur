61a42790efba13bb0fc324887ea87a6d2fbe28f4
Birkir Brynjarsson <birkir@birkir.is>
2018-07-24 17:07:32 +0000

    Move hugur og hond article

--- munstur.php
+++ munstur.php
@@ -23,7 +23,7 @@
             <div class="entry">
 
            <span class="meta">Munstur.is</span>
-           <h2><a href="#" id="fyrirtaekid">Munstur og Menning</a></h2>
+           <h2><a href="#fyrirtaekid" id="fyrirtaekid">Munstur og Menning</a></h2>
 
 
            <div class="entry-content">
@@ -36,19 +36,16 @@
  			</div><!--end entry-content-->
 			</div><!--end entry-->
 
-			<div class="entry">
+			<!-- <div class="entry">
 				<span class="meta"></span>
            		<h2><a href="#" id="jenny">Jenný Karlsdóttir</a></h2>
 
-			<div class="entry-content">
-
-                <p>
-                </p>
-			<!-- <p>Ég er fædd á Akureyri 28. Júlí 1939.</p>
-            <p>Að loknu gagnfræðaprófi frá Núpi í Dýrafirði (1955) stundaði ég nám við Húsmæðraskólann á Laugalandi í Eyjafirði árið eftir. Samhliða barnauppeldi og brauðstriti lauk ég kennaraprófi frá Kennaraháskóla Íslands (1982) og starfaði við kennslu frá 1971 til 2006 á Akureyri og í Eyjafirði. Veturinn 1995-1996 var ég í námsleyfi og lærði myndvefnað í Statens Lærerhögskole í Oslo. Einnig var ég í Fornámsdeild Myndlistarskólans á Akureyri sept-des 1998.</p>
-            <p>Ég hef fjölbreytta og  nokkuð heildstæða handverksþekkingu,  þar sem í kennaranámi mínu tók ég smíðar sem valgrein og hannyrðirnar lærði ég m.a. í húsmæðraskólanum. Svo er ég alin upp á bólsturverkstæði. Faðir minn var með bólsturgerð á neðri hæðinni og móðir mín var körfumublusmiður, menntuð í Noregi. Að auki hefi ég sótt fjöldann allan af handverks og kennaranámskeiðum frá 1967.</p> -->
-			</div>
-			</div>
+			    <div class="entry-content">
+			        <p>Ég er fædd á Akureyri 28. Júlí 1939.</p>
+                    <p>Að loknu gagnfræðaprófi frá Núpi í Dýrafirði (1955) stundaði ég nám við Húsmæðraskólann á Laugalandi í Eyjafirði árið eftir. Samhliða barnauppeldi og brauðstriti lauk ég kennaraprófi frá Kennaraháskóla Íslands (1982) og starfaði við kennslu frá 1971 til 2006 á Akureyri og í Eyjafirði. Veturinn 1995-1996 var ég í námsleyfi og lærði myndvefnað í Statens Lærerhögskole í Oslo. Einnig var ég í Fornámsdeild Myndlistarskólans á Akureyri sept-des 1998.</p>
+                    <p>Ég hef fjölbreytta og  nokkuð heildstæða handverksþekkingu,  þar sem í kennaranámi mínu tók ég smíðar sem valgrein og hannyrðirnar lærði ég m.a. í húsmæðraskólanum. Svo er ég alin upp á bólsturverkstæði. Faðir minn var með bólsturgerð á neðri hæðinni og móðir mín var körfumublusmiður, menntuð í Noregi. Að auki hefi ég sótt fjöldann allan af handverks og kennaranámskeiðum frá 1967.</p>
+			    </div>
+			</div> -->
 
             <div class="entry">
 
@@ -70,6 +67,32 @@
                 <p>Heiður­sviður­kenn­ing Menn­ing­ar­sjóðs er veitt ein­stak­ling­i sem hefur með fram­lagi sínu stutt við og auðgað menn­ing­ar­líf bæj­ar­ins. Í ár varð fyrir valinu handverkskonan Jenný Karlsdóttir en hún hefur lagt mikið af mörkum við að varðveita og upphefja íslenska menningararfinn. Jenný hefur um langa hríð safnað munstrum og gert þau aðgengileg fyrir almenning.</p>
             </div>
             </div>
+
+            <div class="entry">
+
+                <span class="meta">Grein sem birtist í Hugur og Hönd 2018.</span>
+                <h2><a href="#huguroghond2018" id="huguroghond2018">Dagarnir eru aldrei nógu langir</a></h2>
+
+                <div class="entry-head">
+                    <!--<span class="categories"><a href="#">twicet</a>,<a href="#">Startup</a></span>-->
+                    <span class="date">24. júlí 2018</span><br />
+                    <span class="date">Smelltu á myndina og svo á boxið efst í hægra horninu á myndinni til að sjá hana í fullri stærð.</span>
+                    <!--<span class="comments"><a href="#">3 Comments</a></span>-->
+                </div>
+
+                <div class="entry-content"><!--<img class="aligncenter" src="files/contentpic1.jpg" alt="" />-->
+                <p>Einnig er hægt að <a href="images/huguroghond-2018/huguroghond-jenny-2018.pdf">sækja alla greinina sem pdf hér.</a></p>
+                <a href="images/huguroghond-2018/huguroghond-2018-1.jpg" rel="prettyPhoto[altaris]"><img src="images/huguroghond-2018/huguroghond-2018-1_tn.jpg" alt="Grein í Hugur og Hönd 2018, bls.9" width="570" /></a>
+                <a href="images/huguroghond-2018/huguroghond-2018-2.jpg" rel="prettyPhoto[altaris]"><img src="images/huguroghond-2018/huguroghond-2018-2_tn.jpg" alt="Grein í Hugur og Hönd 2018, bls.10" width="570" /></a>
+                <a href="images/huguroghond-2018/huguroghond-2018-3.jpg" rel="prettyPhoto[altaris]"><img src="images/huguroghond-2018/huguroghond-2018-3_tn.jpg" alt="Grein í Hugur og Hönd 2018, bls.11" width="570" /></a>
+                <a href="images/huguroghond-2018/huguroghond-2018-4.jpg" rel="prettyPhoto[altaris]"><img src="images/huguroghond-2018/huguroghond-2018-4_tn.jpg" alt="Grein í Hugur og Hönd 2018, bls.12" width="570" /></a>
+                <a href="images/huguroghond-2018/huguroghond-2018-5.jpg" rel="prettyPhoto[altaris]"><img src="images/huguroghond-2018/huguroghond-2018-5_tn.jpg" alt="Grein í Hugur og Hönd 2018, bls.13" width="570" /></a>
+                <a href="images/huguroghond-2018/huguroghond-2018-6.jpg" rel="prettyPhoto[altaris]"><img src="images/huguroghond-2018/huguroghond-2018-6_tn.jpg" alt="Grein í Hugur og Hönd 2018, bls.14" width="570" /></a>
+                <a href="images/huguroghond-2018/huguroghond-2018-7.jpg" rel="prettyPhoto[altaris]"><img src="images/huguroghond-2018/huguroghond-2018-7_tn.jpg" alt="Grein í Hugur og Hönd 2018, bls.15" width="570" /></a>
+
+
+                </div><!--end entry-content-->
+            </div><!--end entry-->
 
            </div><!-- end inner_content-->
 
